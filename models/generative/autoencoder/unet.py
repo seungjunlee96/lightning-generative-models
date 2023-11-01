eec987fd07168817d787d0306d2b79f624380b4b
Seungjun Lee <lsjj096@gmail.com>
2023-11-01 19:08:18 +0900

    :sparkle: add UNet

--- models/generative/autoencoder/unet.py
+++ models/generative/autoencoder/unet.py
@@ -1,121 +1,151 @@
-"""Code from https://github.com/milesial/Pytorch-UNet"""
+from typing import List, Tuple
+
+import pytorch_lightning as pl
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from torch import Tensor, nn, optim
 
 
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
+class DoubleConvolution(nn.Module):
+    """
+    Double 3x3 convolution with ReLU activation. Optionally includes a MaxPooling layer.
+    """
 
-    def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        use_maxpool: bool = False,
+    ) -> None:
+        super(DoubleConvolution, self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2) if use_maxpool else nn.Identity(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
+        return self.layers(x)
+
+
+class Deconvolution(nn.Module):
+    """
+    Deconvolution followed by a double convolution.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super(Deconvolution, self).__init__()
+        self.deconv = nn.ConvTranspose2d(
+            in_channels, in_channels // 2, kernel_size=2, stride=2
+        )
+        self.double_conv = DoubleConvolution(in_channels, out_channels)
+
+    def forward(self, x: Tensor, skip_connection: Tensor) -> Tensor:
+        x = self.deconv(x)
+        x = torch.cat([x, skip_connection], dim=1)
         return self.double_conv(x)
 
 
-class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
+class Encoder(nn.Module):
+    """
+    Encoder part of the U-Net architecture.
+    """
 
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2), DoubleConv(in_channels, out_channels)
+    def __init__(self, in_channels: int, features_list: List[int]) -> None:
+        super(Encoder, self).__init__()
+
+        layers = [DoubleConvolution(in_channels, features_list[0], use_maxpool=False)]
+        for in_features, out_features in zip(features_list[:-1], features_list[1:]):
+            layers += [DoubleConvolution(in_features, out_features, use_maxpool=True)]
+        self.blocks = nn.ModuleList(layers)
+
+    def forward(self, x: Tensor) -> List[Tensor]:
+        features = []
+        for block in self.blocks:
+            x = block(x)
+            features.append(x)
+        return features
+
+
+class Decoder(nn.Module):
+    """
+    Decoder part of the U-Net architecture.
+    """
+
+    def __init__(self, features_list: List[int], out_channels: int) -> None:
+        super(Decoder, self).__init__()
+
+        features_list = list(reversed(features_list))
+        self.blocks = nn.ModuleList(
+            [
+                Deconvolution(in_features, out_features)
+                for in_features, out_features in zip(
+                    features_list[:-1], features_list[1:]
+                )
+            ]
         )
+        self.head = nn.Conv2d(features_list[0], out_channels, kernel_size=1)
 
-    def forward(self, x):
-        return self.maxpool_conv(x)
+    def forward(self, encoder_features: List[Tensor]) -> Tensor:
+        # Extract the last feature tensor for initial processing in the decoder
+        x = encoder_features.pop()
 
+        # Use the remaining encoder features for skip connections in reverse order
+        for block, skip_connection in zip(self.blocks, encoder_features[::-1]):
+            x = block(x, skip_connection)
 
-class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(
-                in_channels, in_channels // 2, kernel_size=2, stride=2
-            )
-            self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
+        return self.head(x)
 
 
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+class UNet(pl.LightningModule):
+    """
+    U-Net architecture using PyTorch Lightning.
+    """
 
-    def forward(self, x):
-        return self.conv(x)
-
-
-class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        features_list: List[int],
+        lr: float = 1e-3,
+    ) -> None:
         super(UNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
+        self.save_hyperparameters()
+        self.encoder = Encoder(in_channels, features_list)
+        self.decoder = Decoder(features_list, out_channels)
+        self.loss = nn.MSELoss()  # Adjust this as needed for your task
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
-        factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+    def forward(self, x: Tensor) -> Tensor:
+        encoder_features = self.encoder(x)
+        return self.decoder(encoder_features)
 
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+    def _common_step(self, batch: Tuple[Tensor, Tensor], split: str):
+        assert split in ["train", "val"]
+        x, _ = batch
+        x_hat = self(x)
+        loss = self.loss(x_hat, x)
 
-    def use_checkpointing(self):
-        self.inc = torch.utils.checkpoint(self.inc)
-        self.down1 = torch.utils.checkpoint(self.down1)
-        self.down2 = torch.utils.checkpoint(self.down2)
-        self.down3 = torch.utils.checkpoint(self.down3)
-        self.down4 = torch.utils.checkpoint(self.down4)
-        self.up1 = torch.utils.checkpoint(self.up1)
-        self.up2 = torch.utils.checkpoint(self.up2)
-        self.up3 = torch.utils.checkpoint(self.up3)
-        self.up4 = torch.utils.checkpoint(self.up4)
-        self.outc = torch.utils.checkpoint(self.outc)
+        self.log(
+            f"{split}_loss",
+            loss,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+        return loss
+
+    def training_step(self, batch: Tuple[Tensor, Tensor]) -> Tensor:
+        return self._common_step(batch, "train")
+
+    def validation_step(self, batch: Tuple[Tensor, Tensor]) -> Tensor:
+        return self._common_step(batch, "val")
+
+    def configure_optimizers(self) -> optim.Optimizer:
+        optimizer = optim.Adam(
+            self.parameters(),
+            lr=self.hparams.lr,
+        )
+        return optimizer
