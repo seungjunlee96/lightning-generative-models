c4c85a1fff63efb53db1d5604559b636e7a515ed
Seungjun Lee <lsjj096@gmail.com>
2023-11-25 16:03:17 +0900

    :recycle: Refactored configure_strategy for readability

--- utils/lightning_utils.py
+++ utils/lightning_utils.py
@@ -1,34 +1,45 @@
+import multiprocessing
 import os
 from typing import Optional
-import multiprocessing
+
 import torch
-from pytorch_lightning.strategies import Strategy, DDPStrategy, SingleDeviceStrategy
+from pytorch_lightning.strategies import DDPStrategy, SingleDeviceStrategy, Strategy
 
 
 def configure_strategy() -> Strategy:
     """
     Configures the appropriate PyTorch Lightning strategy based on the available hardware.
-    
-    The function automatically detects the environment and sets the strategy accordingly.
-    It supports multiple GPUs on a Linux machine, a single GPU (including M1 Max GPU), and CPU.
+
+    Automatically detects the environment and chooses an optimal strategy.
+    Supports:
+    - Multiple GPUs on a Linux machine (using DDPStrategy).
+    - A single GPU (including M1 Max GPU) (using SingleDeviceStrategy).
+    - CPU (using SingleDeviceStrategy).
 
     Returns:
         Strategy: A PyTorch Lightning strategy object suitable for the detected hardware.
     """
+    # Check for CUDA GPUs availability
     if torch.cuda.is_available():
         num_gpus = torch.cuda.device_count()
+
+        # Use DDPStrategy for multiple GPUs
         if num_gpus > 1:
-            print(f"Using DDPStrategy with {num_gpus} GPUs")
+            print(f"Using DDPStrategy for distributed training on {num_gpus} GPUs.")
             return DDPStrategy(find_unused_parameters=True)
-        else:
-            print("Using SingleDeviceStrategy with 1 GPU")
-            return SingleDeviceStrategy("cuda")
+
+        # Use SingleDeviceStrategy for a single GPU
+        print("Using SingleDeviceStrategy for training on a single GPU.")
+        return SingleDeviceStrategy("cuda")
+
+    # Check for Apple Silicon GPU availability
     elif torch.backends.mps.is_available():
-        print("Using SingleDeviceStrategy with M1 Max GPU")
+        print("Using SingleDeviceStrategy for training on Apple Silicon GPU.")
         return SingleDeviceStrategy("mps")
-    else:
-        print("Using CPU strategy")
-        return SingleDeviceStrategy("cpu")
+
+    # Default to CPU strategy if no GPU is available
+    print("Using SingleDeviceStrategy for training on CPU.")
+    return SingleDeviceStrategy("cpu")
 
 
 def configure_num_workers(num_gpus: Optional[int] = None) -> int:
@@ -36,7 +47,7 @@ def configure_num_workers(num_gpus: Optional[int] = None) -> int:
     Configures the optimal number of workers for PyTorch DataLoader based on the system's resources.
 
     Args:
-        num_gpus (Optional[int]): The number of GPUs available for training. 
+        num_gpus (Optional[int]): The number of GPUs available for training.
                                    If None, the function will try to detect the number of GPUs.
 
     Returns:
@@ -48,7 +59,7 @@ def configure_num_workers(num_gpus: Optional[int] = None) -> int:
         num_gpus = torch.cuda.device_count() if torch.cuda.is_available() else 0
 
     num_cpus = multiprocessing.cpu_count()
-    
+
     # In a DDP setup (multiple GPUs), it's crucial to balance the CPU load across all GPUs.
     # Overloading the CPU with too many workers can lead to bottlenecks in data loading,
     # reducing overall training efficiency.
@@ -58,8 +69,8 @@ def configure_num_workers(num_gpus: Optional[int] = None) -> int:
         num_workers = max(1, num_cpus // (num_gpus * 2))
     else:
         # In single-GPU or CPU-only environments, the strategy differs based on the OS.
-        if os.name == 'posix':  # Linux and macOS
-            if 'linux' in os.uname().sysname.lower():
+        if os.name == "posix":  # Linux and macOS
+            if "linux" in os.uname().sysname.lower():
                 # Linux generally handles multi-threading more efficiently,
                 # allowing us to use more workers per DataLoader
                 num_workers = max(1, num_cpus // 2)
