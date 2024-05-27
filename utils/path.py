ce294477902f4805d9f34fcdf7cf38dccf41f81b
Seungjun Lee <seungjun.lee@vuno.co>
2024-05-27 13:25:09 +0900

    modified data path

--- utils/path.py
+++ utils/path.py
@@ -2,10 +2,11 @@ import sys
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parents[1]
+DATA_ROOT_PATH = PROJECT_ROOT  # modify this to your data root path
 sys.path.append(str(PROJECT_ROOT))
 
 # Data related
-DATASET_PATH = PROJECT_ROOT / "data" / "dataset"
+DATASET_PATH = DATA_ROOT_PATH / "data" / "dataset"
 
 # Experiments and checkpoints related
-EXPERIMENT_DIR = PROJECT_ROOT / "experiments"
+EXPERIMENT_DIR = DATA_ROOT_PATH / "experiments"
